f3a8ab00d9877d30df5b1d47230d4c0e19b31356
IwonaMaurer <plastimex2@wp.pl>
2017-12-15 17:27:52 +0100

    zadanie

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,28 +1,15 @@
 var ContactForm = React.createClass({
-  propTypes: {
-    contact: React.PropTypes.object.isRequired
-  },
-
-  render: function() {
-    return (
-      React.createElement('form', {className: 'contactForm'},
-        React.createElement('input', {
-          type: 'text',
-          placeholder: 'Imię',
-          value: this.props.contact.firstName,
-        }),
-        React.createElement('input', {
-          type: 'text',
-          placeholder: 'Nazwisko',
-          value: this.props.contact.lastName,
-        }),
-        React.createElement('input', {
-          type: 'email',
-          placeholder: 'Email',
-          value: this.props.contact.email,
-        }),
-        React.createElement('button', {type: 'submit'}, "Dodaj kontakt")
-      )
-    )
-  },
+	propTypes: {
+		contact: React.PropTypes.object.isRequired
+	},
+	render: function() {
+		return (
+			<form className={'contactForm form-group form-inline'}>
+				<input className={'form-control'} type={'text'} placeholder={'Imię'} value={this.props.contact.firstName}></input>
+				<input className={'form-control'} type={'text'} placeholder={'Nazwisko'} value={this.props.contact.lastName}></input>
+				<input className={'form-control'} type={'email'} placeholder={'Email'} value={this.props.contact.email}></input>
+				<button className={'btn btn-primary'} type={'submit'}>Dodaj kontakt</button>
+			</form>
+		);
+	},
 })
